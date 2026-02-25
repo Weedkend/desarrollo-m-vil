58d17675c1d165dfe10e7cf7a25e0df87ba933d0
Weedkend <160240681+Weedkend@users.noreply.github.com>
2026-02-24 20:24:34 -0500

    Implement animal hierarchy with mixins in Dart

---
+++ actividad2.dart
@@ -0,0 +1,51 @@
+abstract class Animal {}
+
+abstract class Mamifero extends Animal {}
+
+abstract class Ave extends Animal {}
+
+abstract class Pez extends Animal {}
+
+mixin Volar {
+  void volar() => print('estoy volando');
+}
+
+mixin Caminar {
+  void caminar() => print('estoy caminando');
+}
+
+mixin Nadar {
+  void nadar() => print('estoy nadando');
+}
+
+// Mamíferos
+class Delfin extends Mamifero with Nadar {}
+
+class Murcielago extends Mamifero with Caminar, Volar {}
+
+class Gato extends Mamifero with Caminar {}
+
+// Aves
+class Pato extends Ave with Caminar, Volar, Nadar {}
+
+class Paloma extends Ave with Caminar, Volar {}
+
+// Peces
+class Tiburon extends Pez with Nadar {}
+
+class PezVolador extends Pez with Nadar, Volar {}
+
+void main() {
+
+  final flipper = Delfin();
+  flipper.nadar();
+
+  final batman = Murcielago();
+  batman.caminar();
+  batman.volar();
+
+  final pato = Pato();
+  pato.caminar();
+  pato.volar();
+  pato.nadar();
+}
